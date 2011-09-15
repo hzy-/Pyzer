cea32190035d8da8401c48bf89706ec11ed8aa2e
Jacob Haslehurst <jacob@haslehurst.net>
2011-09-16 00:43:03 +0800

    I think everything is now working, ready to deploy

--- pyzer.py
+++ pyzer.py
@@ -4,15 +4,12 @@ import urllib
 import Image
 import cStringIO
 import redis
+import cPickle as pickle
 
 #defining settings
 redis_cache = redis.Redis(host='localhost', port=6379, db=711)
 
 #routes and classes
-@route('/hi/:name')
-def hi(name):
-	return 'Hello %s!' % name
-
 @route('/resize')
 def resize():
 	if request.GET.get('src'):
@@ -25,26 +22,35 @@ def resize():
 		width = int(request.GET.get('w'))
 	else:
 		width = 2000
-		w_default = True
 
 	if request.GET.get('h'):
 		height = int(request.GET.get('h'))
 	else:
 		height = 2000
-		if w_default == True:
-			return 'include at lest one size parameter'
-	image_id = str(image_url)+'_'+str(width)+'_'+str(height)
-	print image_id
-	if redis_cache.exists(image_id):
-		image_return = redis_cache.get(image_id)
+
+	r_image_id = 'r_'+image_url+'_'+str(width)+'_'+str(height)
+	o_image_id = 'o_'+image_url
+	print 'getting up to redis'
+	if redis_cache.exists(r_image_id):
+		print 'in the resize cache'
+		image_return = redis_cache.get(r_image_id)
 	else:
+		print 'hasn\'t hit the resize cache'
 		#opens image
-		file_from_url = urllib.urlopen(image_url)
-		file_contents = cStringIO.StringIO(file_from_url.read())
+		if redis_cache.exists(o_image_id):
+			print 'hitting the cache', o_image_id
+			file_contents = cStringIO.StringIO(redis_cache.get(o_image_id))
+		else:
+			file_from_url = urllib.urlopen(image_url)
+			file_contents = cStringIO.StringIO(file_from_url.read())
+			print 'starting to cache original image'
+			redis_cache.set(o_image_id, file_contents.getvalue())
+
 		image = Image.open(file_contents)
 
 		#does resizing to said image
-		#...
+		size = [width, height]
+		image.thumbnail(size, Image.ANTIALIAS)
 
 		#preps image for response
 		image_out = cStringIO.StringIO()
@@ -52,7 +58,7 @@ def resize():
 		image_return = image_out.getvalue()
 		
 		#saves image to cache
-		redis_cache.set(image_id, image_return)
+		redis_cache.set(r_image_id, image_return)
 
 	response.content_type = 'image/jpg'
 	return image_return
--- readme.md
+++ readme.md
@@ -5,7 +5,13 @@ It's creation has come out of my need for a simple way to resize images on the f
 #Technologies
 * Bottle web framework for the HTTP request handling
 * Python Image Library (PIL) for the resizing of images
-* Some sort of caching (Memcache or Redis)
+* Redis for caching
 
 #Implementation
 I plan to be using this for my [Weldr](http://weldr.me) social network, as a way of sane-ly displaying images from a foreign source.
+
+#Requirements
+* [Bottle](https://github.com/andymccurdy/redis-py)
+* [Reddis-Py](https://github.com/andymccurdy/redis-py)
+* [Redis](http://redis.io)
+* [Python Image Library](https://github.com/andymccurdy/redis-py)
