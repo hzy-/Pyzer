870485174a6d8f41ca4b52517c4fbff2ca9c9576
Jacob Haslehurst <jacob@haslehurst.net>
2011-09-17 18:19:44 +0400

    now daemonizes

--- pyzer.py
+++ pyzer.py
@@ -1,6 +1,3 @@
-#gevent up in this
-from gevent import monkey; monkey.patch_all()
-
 #imports
 from bottle import route, run, request, response, abort
 import urllib
@@ -8,6 +5,7 @@ import Image
 import cStringIO
 import redis
 import cPickle as pickle
+import daemon
 
 #defining settings
 redis_cache = redis.Redis(host='localhost', port=6379, db=711)
@@ -35,20 +33,15 @@ def resize():
 
 	r_image_id = 'r_'+image_url+'_'+str(width)+'_'+str(height)
 	o_image_id = 'o_'+image_url
-	print 'getting up to redis'
 	if redis_cache.exists(r_image_id):
-		print 'in the resize cache'
 		image_return = redis_cache.get(r_image_id)
 	else:
-		print 'hasn\'t hit the resize cache'
 		#opens image
 		if redis_cache.exists(o_image_id):
-			print 'hitting the cache', o_image_id
 			file_contents = cStringIO.StringIO(redis_cache.get(o_image_id))
 		else:
 			file_from_url = urllib.urlopen(image_url)
 			file_contents = cStringIO.StringIO(file_from_url.read())
-			print 'starting to cache original image'
 			redis_cache.set(o_image_id, file_contents.getvalue())
 
 		image = Image.open(file_contents)
@@ -59,12 +52,13 @@ def resize():
 
 		#preps image for response
 		image_out = cStringIO.StringIO()
-		image.save(image_out, "JPEG")
+		image.save(image_out, "JPEG", quality=80)
 		image_return = image_out.getvalue()
 		
 		#saves image to cache
 		redis_cache.set(r_image_id, image_return)
 
+	print r_image_id
 	response.content_type = 'image/jpg'
 	response.set_header('Cache-Control', 'max-age=31536000')
 	response.set_header('Last-Modified', 'Wed, 11 Jan 1984 08:00:00 GMT')
@@ -72,4 +66,10 @@ def resize():
 	
 
 #let's run this
-run(server='gunicorn')
+context = daemon.DaemonContext(
+    working_directory='.',
+    umask=0o002,
+    )
+
+with context:
+	run(server='tornado', host='0.0.0.0', port=9998)
